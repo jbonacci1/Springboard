5a4d2743e34e71aadbfc894d2ec9700c3609bf9c
Jack Bonacci <36173474+jbonacci1@users.noreply.github.com>
2018-07-24 17:54:20 -0700

    Add files via upload
    Training dataset for Air Bnb Project.  Descriptive statistics included.

---
+++ train_doc.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+train = pd.read_csv('/Users/jackbonacci/Downloads/train.csv')
+
+
+# In[6]:
+
+
+train.describe(include='all')
+
